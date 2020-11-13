360b5f10a6f52494cea9bda3e6da069ec5a2fd7b
github motokiS2 <pmochad14@gmail.com>
2020-11-13 11:44:40 +0900

    ch-2-2

---
+++ ch02/2-2/Fence Repair.py
@@ -0,0 +1,29 @@
+N = 5
+L = [3, 4, 5, 1, 2]
+
+ans = 0
+
+while N > 1:
+    mii1 = 0
+    mii2 = 1
+    if L[mii1] > L[mii2]:
+        L[mii1], L[mii2] = L[mii2], L[mii1]
+
+    for i in range(2, N):
+        if L[i] < L[mii1]:
+            mii2 = mii1
+            mii1 = i
+        elif L[i] < L[mii2]:
+            mii2 = i
+
+    t = L[mii1] + L[mii2]
+    ans += t
+
+    if mii1 == N - 1:
+        mii1, mii2 = mii2, mii1
+
+    L[mii1] = t
+    L[mii2] = L[N - 1]
+    N -= 1
+
+print(ans)
