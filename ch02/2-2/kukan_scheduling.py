20af388232cf70ed6a3967e2f290c18baaec51bc
github motokiS2 <pmochad14@gmail.com>
2020-11-13 10:53:37 +0900

    kukan_scheduling

--- ch02/2-2/kukan_scheduling.py
+++ ch02/2-2/kukan_scheduling.py
@@ -1,12 +1,22 @@
-n = 5
-s = [1, 2, 4, 6, 8]
-t = [3, 5, 7, 9, 10]
+N = 3
+S = [1, 2, 4, 6, 8]
+T = [3, 5, 7, 9, 10]
 
-MAX_N = 1e+6
+itv = [[0, 0] for _ in range(N)]
 
-itv = [(0, 0)] * MAX_N
+for i in range(N):
+    itv[i][0] = T[i]
+    itv[i][1] = S[i]
 
-for i in range(n):
-    itv[i][0] = t[i]
-    itv[i][1] = s[i]
+itv = sorted(itv)
 
+print(itv)
+
+ans = 0
+t = 0
+for i in range(N):
+    if t < itv[i][1]:
+        ans += 1
+        t = itv[i][0]
+
+print(ans)
